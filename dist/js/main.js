1034d66bcf21e5ab6567d48089d977c687040c36
Dominique Patino <DPatino07@hotmail.com>
2020-09-16 12:30:54 -0700

    Finished Mockup of Website Landing Page

---
+++ dist/js/main.js
@@ -0,0 +1,21 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menuBtn.classList.toggle('close');
+    menu.classList.toggle('show');
+    menuNav.classList.toggle('show');
+    menuBranding.classList.toggle('show');
+    navItems.forEach(item => item.classList.toggle('show'));
+
+    showMenu = !showMenu;
+}
